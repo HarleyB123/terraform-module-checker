bb2c673854de3f0ab7362704d596a8fd3be92105
HarleyB123 <Harley.Bates@kpmg.co.uk>
2021-03-28 12:26:05 +0100

    test.tf

---
+++ test.tf
@@ -0,0 +1,13 @@
+module "eg_prod_bastion_label" {
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=0.23.0"
+  namespace  = "eg"
+  stage      = "prod"
+  name       = "bastion"
+  attributes = ["public"]
+  delimiter  = "-"
+
+  tags = {
+    "BusinessUnit" = "XYZ",
+    "Snapshot"     = "true"
+  }
+}
