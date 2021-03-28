e94a4833a6f14193ea829cc79a48c930a2d43bda
HarleyB123 <Harley.Bates@kpmg.co.uk>
2021-03-28 13:18:24 +0100

    yay

--- terraform_module_checker.sh
+++ terraform_module_checker.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -euo pipefail
-ORANGE='\u001b[31;1m'
 MESSAGE=''
 WORKING_DIRECTORY=$1
 cd $1
@@ -11,12 +10,11 @@ then
     cd .terraform/modules
     find . -maxdepth 1 -mindepth 1 -type d | while read dir; do
       cd $dir
-      MESSAGE="${MESSAGE} ${ORANGE} INFO - The latest version of module ${dir#./} is tag $(git describe --tags --abbrev=0). Please ensure you are using this version.\n"
+      MESSAGE="${MESSAGE} INFO - The latest version of module ${dir#./} is tag $(git describe --tags --abbrev=0). Please ensure you are using this version.\n"
       echo "::set-output name=MESSAGE::$(echo -e "${MESSAGE}")"
-      echo -e "${MESSAGE}"
       cd ..
     done
 else
-    echo "::set-output name=MESSAGE::$(echo -e "${ORANGE} INFO - No modules found in code")"
+    echo "::set-output name=MESSAGE::$(echo -e "INFO - No modules found in code")"
 fi
 
