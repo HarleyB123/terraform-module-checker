fa8ca6a1c464803c10b52ac3eb3108a9d4bbe0f2
HarleyB123 <Harley.Bates@kpmg.co.uk>
2021-03-28 12:59:49 +0100

    terraform_module_checker.sh

--- terraform_module_checker.sh
+++ terraform_module_checker.sh
@@ -14,8 +14,8 @@ then
       MESSAGE="${MESSAGE} ${ORANGE} INFO - The latest version of module ${dir#./} is tag $(git describe --tags --abbrev=0). Please ensure you are using this version.\n"
       cd ..
     done
-    echo "::set-output name=MESSAGE::echo -e "${MESSAGE}""
+    echo "::set-output name=MESSAGE::$(echo -e "${MESSAGE}")"
 else
-     echo "::set-output name=MESSAGE::echo -e "${ORANGE} INFO - No modules found in code""
+    echo "::set-output name=MESSAGE::$(echo -e "${ORANGE} INFO - No modules found in code")"
 fi
 
