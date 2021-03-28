c260a8d0529e334819a0eb6a10b39bfba5700fa5
HarleyB123 <Harley.Bates@kpmg.co.uk>
2021-03-28 12:20:20 +0100

    adding terraform module checker

---
+++ terraform_module_checker.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -euo pipefail
+ORANGE='\u001b[31;1m'
+cd ${{ inputs.working_directory }}
+sed -i 's/\?ref.*/"/' *.tf
+terraform get -update
+if [ -d ".terraform/modules" ]
+then
+    cd .terraform/modules
+    find . -maxdepth 1 -mindepth 1 -type d | while read dir; do
+      cd $dir
+      echo -e "${ORANGE} INFO - The latest version of module ${dir#./} is tag $(git describe --tags --abbrev=0). Please ensure you are using this version."
+      cd ..
+    done
+else
+    echo -e "${ORANGE} INFO - No modules found in code"
+fi
+
