ff05b9ccc37f09248cac87a71fb09e7ddba08270
HarleyB123 <Harley.Bates@kpmg.co.uk>
2021-03-30 08:51:52 +0100

    no orange

--- terraform_module_checker.sh
+++ terraform_module_checker.sh
@@ -10,7 +10,7 @@ then
     cd .terraform/modules
     find . -maxdepth 1 -mindepth 1 -type d | while read dir; do
       cd $dir
-      MESSAGE="${MESSAGE} ${ORANGE} INFO - The latest version of module ${dir#./} is tag $(git describe --always --tags --abbrev=0). Please ensure you are using this version.\n"
+      MESSAGE="${MESSAGE} INFO - The latest version of module ${dir#./} is tag $(git describe --always --tags --abbrev=0). Please ensure you are using this version.\n"
       echo "::set-output name=MESSAGE::echo -e "${MESSAGE}""
       cd ..
     done
