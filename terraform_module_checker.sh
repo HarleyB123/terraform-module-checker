fc5ad830d88067b718fdd4db7ae24d5c5d0233ee
HarleyB123 <Harley.Bates@kpmg.co.uk>
2021-03-31 19:01:28 +0100

    testing

--- terraform_module_checker.sh
+++ terraform_module_checker.sh
@@ -10,7 +10,6 @@ then
     cd .terraform/modules
     find . -maxdepth 1 -mindepth 1 -type d | while read dir; do
       cd $dir
-      ls -la
       if [ -d ".git" ]
       then
           MESSAGE="${MESSAGE} The latest version of module ${dir#./} is tag $(git describe --always --tags --abbrev=0). Please ensure you are using this version."
@@ -19,8 +18,8 @@ then
       else
           MESSAGE="${MESSAGE} Unable to get tag for module ${dir#./}."
           echo "::set-output name=MESSAGE::"${MESSAGE}"\n"
+          cd ..
       fi
-      cd ..
     done
 else
     echo "::set-output name=MESSAGE::$(echo -e "No modules found in code")"
